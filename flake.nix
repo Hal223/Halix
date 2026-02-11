15cf6ff9cbf5684e7967e7dfdb13b7593b8c7cd3
Holden Prather <jprather223@gmail.com>
2026-02-11 15:18:29 -0700

    Build:  - /nix/store/s5p9fa6a43mn36ln507ymicw5ir6lq86-nixos-system-halix-26.05.20251228.c0b0e0f

--- flake.nix
+++ flake.nix
@@ -3,10 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    fresh.inputs.nixpkgs.follows = "nixpkgs";
+
     wrappers.url = "github:lassulus/wrappers";
     wrappers.inputs.nixpkgs.follows = "nixpkgs";
+
+    fresh.inputs.nixpkgs.follows = "nixpkgs";
     fresh.url = "github:sinelaw/fresh";
+
     yazi-plugins = {
       url = "github:yazi-rs/plugins";
       flake = false;
