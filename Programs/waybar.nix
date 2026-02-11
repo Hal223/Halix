72a6c973f4764b3d26c06d618614bd77cac82b4f
Holden Prather <jprather223@gmail.com>
2026-02-11 15:22:56 -0700

    Build:  -

--- Programs/waybar.nix
+++ Programs/waybar.nix
@@ -3,8 +3,8 @@
   pkgs,
   ...
 }: let
-  waybar-wrapped = inputs.wrappers.lib.wrapPackage pkgs {
-    package = pkgs.waybar; # Explicitly passing the 'package' argument
+  # Try positional: pkgs -> the package to wrap -> the config set
+  waybar-wrapped = inputs.wrappers.lib.wrapPackage pkgs pkgs.waybar {
     flags = [
       "-c ${./waybar-config.json}"
       "-s ${./style.css}"
