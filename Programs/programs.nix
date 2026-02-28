cc72a92c28b7b783577114819c4983ffc0d84717
Holden Prather <jprather223@gmail.com>
2026-02-27 21:37:01 -0700

    Build:  - /nix/store/zvc9yg7xnm85w1j7rz43msxrvydjy4g0-nixos-system-halix-26.05.20251228.c0b0e0f

--- Programs/programs.nix
+++ Programs/programs.nix
@@ -69,6 +69,18 @@
     });
   '';
 
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "gnome-polkit-agent";
+    wantedBy = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
   # 4. Global Packages
   environment.systemPackages = with pkgs; [
     # --- Desktop & Productivity ---
