f716bfeba8ef29d21b7898f7286d2db2be052ab2
Holden Prather <jprather223@gmail.com>
2026-03-13 19:45:10 -0600

    Build:  - /nix/store/mcywb8q0mr5ykbca0l4vjy62gi91189f-nixos-system-halix-26.05.20251228.c0b0e0f

--- Programs/programs.nix
+++ Programs/programs.nix
@@ -24,13 +24,6 @@
     configDir = "/home/hal/.config/syncthing";
   };
 
-  # enable Sway window manager
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-    package = swayHomeless;
-  };
-
   programs.git = {
     enable = true;
     config = {
