f81a65b986bc5dccdb0f490c5b73bb2310e89ed1
Holden Prather <jprather223@gmail.com>
2026-06-20 10:47:02 -0600

    Build:  - /nix/store/rxgcnah1f3115v5qni5znbyy4cr9xzm1-nixos-system-halix-26.05.20251228.c0b0e0f

--- configuration.nix
+++ configuration.nix
@@ -18,8 +18,6 @@
     "flakes"
   ];
   system.stateVersion = "26.05";
-  nix.settings.substituters = ["https://nix-community.cachix.org"];
-  nix.settings.trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
