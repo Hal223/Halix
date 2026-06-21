c40779fcfefbdbc5b9a39d8c35680c52b0b2d14a
Holden Prather <jprather223@gmail.com>
2026-06-21 14:58:58 -0600

    Cleanup: Remove legacy root configuration files and unused Programs directory

--- Programs/mulvad-vpn.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
-  # VPN solution moving away from PIA as hassle to config in nix 2026-01-17
-  services.mullvad-vpn.enable = true;
-  services.mullvad-vpn.package = pkgs.mullvad-vpn;
-}
