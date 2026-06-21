156234412ab9ef033e246e630679867d3323fb68
Holden Prather <jprather223@gmail.com>
2026-06-21 12:27:43 -0600

    Build:  - /nix/store/s3rm2swq12s0h835f7q84jn8kz3ysvp1-nixos-system-halix-26.11.20260616.567a49d

--- display.nix
+++ display.nix
@@ -17,18 +17,11 @@
     slurp # screenshot functionality
     wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
     mako # notification system developed by swaywm maintainer
+    gnome-keyring # Or use 'pass' or 'kwallet' depending on your desktop
+    xdg-utils # Essential for opening external URLs/links
   ];
 
   # Enable the gnome-keyring secrets vault.
   # Will be exposed through DBus to programs willing to store secrets.
   #services.gnome.gnome-keyring.enable = true;
-
-  # In /etc/nixos/configuration.nix or home.nix
-  environment.systemPackages = with pkgs; [
-    xdg-utils # Essential for opening external URLs/links
-    gnome-keyring # Or use 'pass' or 'kwallet' depending on your desktop
-  ];
-
-  # For Home Manager users:
-  services.gnome-keyring.enable = true;
 }
