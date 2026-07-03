86872a63e6250da902ded7a108949c4691ebcf13
Holden Prather <jprather223@gmail.com>
2026-07-03 12:52:01 -0600

    Build:  - /nix/store/9idsb4l32p8nvswz31dx6pc6i4mqrlx5-nixos-system-halix-26.11.20260629.b5aa0fb

--- modules/programs/steam.nix
+++ modules/programs/steam.nix
@@ -12,7 +12,10 @@
         with pkgs; [
           # Add extra dependencies here if needed (e.g., openssl, nghttp2)
         ];
-      extraProfile = "export DBUS_FATAL_WARNINGS=0";
+      extraProfile = ''
+        export DBUS_FATAL_WARNINGS=0
+        export GDK_BACKEND=x11
+      '';
     };
   };
 }
