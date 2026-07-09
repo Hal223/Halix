256324545efd4d730e7d42ea648cc7e078f6ae62
Holden Prather <jprather223@gmail.com>
2026-07-09 16:28:14 -0600

    Build:  - /nix/store/bkvk12ap2qb53yg1kqsxcdj90hr5yk82-nixos-system-halix-26.11.20260705.d407951

--- modules/desktop/ags/widget/Bar.tsx
+++ modules/desktop/ags/widget/Bar.tsx
@@ -24,7 +24,7 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
           hexpand
           halign={Gtk.Align.CENTER}
         >
-          <label label="Welcome to AGS!!!!!!!!" />
+          <label label="Welcome to AGS!" />
         </button>
         <box $type="center" />
         <menubutton $type="end" hexpand halign={Gtk.Align.CENTER}>
