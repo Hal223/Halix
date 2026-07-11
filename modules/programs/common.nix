7b4e80609d1883603a8cd9974742117161533e85
Holden Prather <jprather223@gmail.com>
2026-07-10 18:05:42 -0600

    Build:  - /nix/store/mz2dbnkpyb9i3jy1wda5gvnch0n2g1lw-nixos-system-halix-26.11.20260705.d407951

--- modules/programs/common.nix
+++ modules/programs/common.nix
@@ -41,5 +41,13 @@
     usbutils
     smartmontools
   ];
-  services.awww.enable = true;
+  systemd.user.services.awww-daemon = {
+    description = "Awww Wallpaper Daemon";
+    wantedBy = ["graphical-session.target"];
+    partOf = ["graphical-session.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.awww}/bin/awww-daemon";
+      Restart = "on-failure";
+    };
+  };
 }
